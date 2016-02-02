d30f9f10b675496dc4c2252bd73fe1640ed52127
Andreas Kyritsakis <andreas.kyritsakis@helsinki.fi>
2016-02-02 19:39:04 +0200

    Finalized Gamow function. Seems to work perfectly. Todo: start writing GTF function

--- Emission/test.f90
+++ Emission/test.f90
@@ -1,11 +1,17 @@
 program test
 
-use emission, only: elec_emit
 
-double precision::J
+use emission, only: Gamow_general
+implicit none
 
-J=elec_emit(5.d0,4.5d0,5.d0,0.d0)
 
-print *, J
+double precision::G(2),F=0.5d0
+integer:: i
+
+do i=1,50
+	G=Gamow_general(F,4.5d0,7.d0)
+	F=F+0.3d0
+enddo
+
 
 end program
