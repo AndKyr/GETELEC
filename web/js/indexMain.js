a14737e327717eba073ef41498d587e4f848d73b
rorychatt <pavel.rinne@gmail.com>
2022-09-08 11:47:31 +0300

    event listener cleanup

--- web/js/indexMain.js
+++ web/js/indexMain.js
@@ -1,5 +1,3 @@
-import { getButtonsForMain } from "./eventListeners.js"
-
 function main() {
 
     getButtonsForMain();
@@ -7,3 +5,39 @@ function main() {
 }
 
 main();
+
+
+function getButtonsForMain() {
+
+	let ivDataBtn = document.getElementById("ivDataBtn");
+	let threeDGraphingBtn = document.getElementById("emissionDataBtn");
+	let documentationBtn = document.getElementById("documentationBtn");
+
+	ivDataBtn.addEventListener("click", goToIvCalculationPage);
+	threeDGraphingBtn.addEventListener("click", goToThreeDGraphingPage);
+	documentationBtn.addEventListener("click", goToDocumentationPage);
+}
+
+function getButtonsForIvCalculations() {
+	let logo = document.getElementById("logo");
+
+	logo.addEventListener("click", window.location.href="./index.html");
+}
+
+function getButtonsForDocumentration() {
+	let logo = document.getElementById("logo");
+
+	logo.addEventListener("click", window.location.href = "./index.html");
+}
+
+function goToIvCalculationPage() {
+	window.location.href = "./ivCalculations.html";
+}
+
+function goToThreeDGraphingPage() {
+	window.location.href = "./emissionCalc.html";
+}
+
+function goToDocumentationPage() {
+	window.location.href = "./briefDoc.html";
+}
