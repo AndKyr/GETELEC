f2746f9dc11ceb9bec6eab56f68e71cf46a13e7c
rorychatt <pavel.rinne@gmail.com>
2022-11-29 10:55:04 +0200

    web folder pushed

---
+++ web/python/emissionCalc.py
@@ -0,0 +1,163 @@
+from distutils.file_util import write_file
+import json
+from math import gamma
+import os
+import sys
+import numpy as np
+
+mainpath,filename = os.path.split(os.path.realpath(__file__))
+
+emissionpath,mainfolder = os.path.split(mainpath)
+emissionpath,mainfolder = os.path.split(emissionpath)
+
+pythonpath = emissionpath + '/python'
+sys.path.append(pythonpath)
+
+from getelec_online import current_metal_emitter, heat_metal_emitter, spectrum_metal_emitter
+from getelec_online import current_semiconductor_emitter, heat_semiconductor_emitter, spectrum_semiconductor_emitter
+
+def convertInput():
+
+    dataInArr = (sys.argv[1])
+
+    data = json.dumps(dataInArr)
+    data = data.split("}")
+    data = data[0].split("{")[1]
+    data = data.split("]")
+
+    lines = []
+    result = []
+
+    for line in data:
+
+        lined = line.split(":")[1:]
+
+        for linedd in lined:
+
+            linedd = linedd[1:]
+            lines.append(linedd)
+
+    for line in lines:
+
+        if "," in line:
+
+            _line = line.split(",")
+            line = []
+
+            for el in _line:
+
+                if el[0] == "[":
+                    el = el[1:]
+
+                line.append(float(el))
+
+            result.append(line)
+            
+        else:
+            
+            if line[0] == "[":
+                line = line[1:]
+
+            result.append([float(line)])
+    
+    return result
+
+def main():
+
+    data = convertInput()
+    
+    materialType = (str(data[0][0]))[0]
+    sweepParam = str(data[1][0])[0]
+    field = data[2]
+    radius = data[3]
+    wf = data[4]
+    temp = data[5]
+
+    ec = data[6][0]
+    ef = data[7][0]
+    eg = data[8][0]
+
+    gammaMetal = data[9]
+    gammaSemi = data[10]
+
+    me = data[11][0]
+    mp = data[12][0]
+
+    calculateEC = (str(data[13][0]))[0]
+    calculateES = (str(data[15][0]))[0]
+    calculateNH = (str(data[14][0]))[0]
+
+    data1 = []
+    data2 = []
+    data3 = []
+    data4 = []
+    data5 = []
+    data6 = []
+    data3e = []
+    data6e = []
+
+    if sweepParam == "2":
+
+        sweepParam = "field"
+
+    elif sweepParam == "3":
+
+        sweepParam = "radius"
+
+    elif sweepParam == "4":
+
+        sweepParam = "wf"
+
+    elif sweepParam == "5":
+
+        sweepParam = "temp"
+
+    if materialType == "1":
+
+        if calculateEC == "1":
+            
+            data1 = current_metal_emitter(field, radius, gammaMetal, wf, temp).tolist()
+        
+        if calculateNH == "1":
+
+            data2 = heat_metal_emitter(field, radius, gammaMetal, wf, temp).tolist()
+
+        if calculateES == "1":
+
+            energies, electronCounts = spectrum_metal_emitter(field, radius, gammaMetal, wf, temp)
+
+            data3 = []
+            data3e = []
+
+            for energy in energies:
+                data3.append(energy.tolist())
+
+            for electronCount in electronCounts:
+                data3e.append(electronCount.tolist())
+            
+    elif materialType == "2":
+
+        if calculateEC == "1":
+
+            data4 = current_semiconductor_emitter(field, radius, gammaSemi, ec, ef, eg, temp, me, mp).tolist()
+
+        if calculateNH == "1":  
+              
+            data5 = heat_semiconductor_emitter(field, radius, gammaSemi, ec, ef, eg, temp, me, mp).tolist()
+  
+        if calculateES == "1":
+            energy, electronCount = spectrum_semiconductor_emitter(field, radius, gammaSemi, ec, ef, eg, temp, me, mp).tolist()
+            data6 = energy.tolist()
+            data6e = electronCount.tolist()
+
+    outdata = {"materialType": materialType, "sweepParam": sweepParam,
+        "field": field, "radius": radius, "work_function": wf, "temperature": temp,
+        "ec": ec, "ef": ef, "eg": eg, "gammaMetal": gammaMetal, "gammaSemi": gammaSemi,
+        "me": me, "mp": mp, "metalEC": data1, "metalNH": data2, "metalESenergy": data3,
+        "metalESelcount": data3e, "semiEC": data4, "semiNH": data5, "semiESenergy": data6,
+        "semiESelcount": data6e
+    }
+
+    print(json.dumps(outdata))
+
+main()
---
+++ web/python/ivCalc.py
@@ -0,0 +1,119 @@
+import cProfile
+import pstats
+import json
+import os
+import sys
+import numpy as np
+
+def convertInput():
+
+    dataInArr = (sys.argv[1])
+
+    data = json.dumps(dataInArr)
+    data = data.split("}")
+    data = data[0].split("{")[1]
+    data = data.split("]")
+
+    lines = []
+    result = []
+
+    for line in data:
+
+        lined = line.split(":")[1:]
+
+        for linedd in lined:
+
+            linedd = linedd[1:]
+            lines.append(linedd)
+
+    for line in lines:
+
+        if "," in line:
+
+            _line = line.split(",")
+            line = []
+
+            for el in _line:
+
+                if el[0] == "[":
+                    el = el[1:]
+
+                line.append(float(el))
+
+            result.append(line)
+            
+        else:
+
+            if len(line) > 0:
+
+                if line[0] == "[":
+
+                    line = line[1:]
+
+                result.append([float(line)])
+
+    return data
+
+dataInArr = convertInput()
+
+print(f'Converted input. Now doing calcs')
+
+mainpath,filename = os.path.split(os.path.realpath(__file__))
+
+emissionpath,mainfolder = os.path.split(mainpath)
+emissionpath,mainfolder = os.path.split(emissionpath)
+
+pythonpath = emissionpath + '/python'
+sys.path.append(pythonpath)
+
+from getelec_online import fit_data
+
+outdata = {}
+
+def main():
+
+    #print(json.dumps(dataInArr))
+
+    xdata = 1./np.array(dataInArr[0])
+    ydata = np.log(np.array(dataInArr[1]))
+    workFunction = float(dataInArr[2][0])
+
+    xplot, xplot_th, yth, beta, radius, sigmaAeff = fit_data(xdata, ydata, workFunction, mode = "simple")
+
+    outdata = { "type": "ivCalc",
+                "xplot_mrk": xplot.tolist(), "yplot_mrk": dataInArr[1], \
+                "xplot_line": xplot_th.tolist(), "yplot_line": yth.tolist(), \
+                "beta": beta, "sigma_Aeff": sigmaAeff, \
+                "xAxisUnit": "1 / (Local Field [V/nm])", "yAxisUnit": "Current [Amps]"}
+
+    print(json.dumps(outdata))
+
+def mainWithRadius():
+
+    #print(json.dumps(dataInArr))
+
+    xdata = 1./np.array(dataInArr[0])
+    ydata = np.log(np.array(dataInArr[1]))
+    workFunction = float(dataInArr[2][0])
+
+    xplot, xplot_th, yth, beta, radius, sigmaAeff = fit_data(xdata, ydata, workFunction, mode = "withRadius")
+
+    outdata = { "type": "ivCalc",
+                "xplot_mrk": xplot.tolist(), "yplot_mrk": dataInArr[1], \
+                "xplot_line": xplot_th.tolist(), "yplot_line": yth.tolist(), \
+                "beta": beta, "Radius": radius, "sigma_Aeff": sigmaAeff, \
+                "xAxisUnit": "1 / (Local Field [V/nm])", "yAxisUnit": "Current [Amps]"}
+
+    print(json.dumps(outdata))
+
+with cProfile.Profile() as pr:
+
+    if float(dataInArr[3][0]) == 0:
+        main()
+    if float(dataInArr[3][0]) == 1:
+        mainWithRadius()
+
+#stats = pstats.Stats(pr)
+#stats.sort_stats(pstats.SortKey.TIME)
+#stats.print_stats()
+#stats.dump_stats(filename='ivCalcProfiling.prof')
