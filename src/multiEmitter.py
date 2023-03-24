6aa2c6d1ad24f6bc3011179f8a1a7bf941610429
Andreas Kyritsakis <akyritsos1@gmail.com>
2023-03-24 15:32:21 +0200

    make multiEmitter work with new getelec

--- src/multiEmitter.py
+++ src/multiEmitter.py
@@ -1,72 +1,59 @@
 
-import getelec_mod as gt
+import getelec as gt
 import numpy as np
 
 import matplotlib.pyplot as plt
 
 class MultiEmitter():
     
-    def __init__(self, em = gt.emission_create(), N = 1, mu_h = 100., std_h = 10., \
-                    mu_r = 10., std_r = 1.):
-        self.emitter = em
-        self.emitter.approx = 1
-        self.N_emitters = N
-        self.mu_height = mu_h
-        self.sigma_height = std_h
-        self.mu_radii = mu_r
-        self.sigma_radii = std_r
+    def __init__(self, emitterModel = gt.GETELECModel(), numberOfEmitters = 128, averageHeight = 300, stdHeight = 30, averageRadius = 15, stdRadius = 3):
+        self.emitterModel = emitterModel
+        self.numberOfEmitters = numberOfEmitters
+        self.averageHeight = averageHeight
+        self.stdHeight = stdHeight
+        self.averageRadius = averageRadius
+        self.stdRadius = stdRadius
+
+        self.currents = np.zeros(numberOfEmitters)
+        self.betas = np.zeros(numberOfEmitters)
+        self.radii = np.zeros(numberOfEmitters)
         
-        
-    def set_emitters(self, radii, heights = np.array([]), betas = np.array([])):
-        self.radii = radii
-        
-        if(not heights.any()):
-            self.betas = betas
-            self.heights = betas * radii
-        else:
-            self.betas = heights / radii
-            self.heights = heights
-        
-        self.N_emitters = len(self.betas)
-        
-        self.currents = np.copy(self.betas) * 0.
-        self.areas = np.copy(self.currents)
-        
+       
         
         
     def getEmitters(self, maxBetaValue = 1.e50, minBetaValue = 1., heightDistribution = 'normal', radiiDistribution = 'lognormal', appendMaxValues = True):
         
         if (heightDistribution == 'lognormal'):
-            mu = np.log(self.mu_height**2/np.sqrt(self.sigma_height**2 + self.mu_height**2))
-            sigma = np.sqrt(np.log(1+self.sigma_height**2/self.mu_height**2))
-            heights = np.random.lognormal(mu, sigma, self.N_emitters)
+            mu = np.log(self.averageHeight**2/np.sqrt(self.stdHeight**2 + self.averageHeight**2))
+            sigma = np.sqrt(np.log(1+self.stdHeight**2/self.averageHeight**2))
+            heights = np.random.lognormal(mu, sigma, self.numberOfEmitters)
         elif(heightDistribution == 'normal'):
-            mu = self.mu_height
-            sigma = self.sigma_height
-            heights = np.random.normal(mu, sigma, self.N_emitters)
+            mu = self.averageHeight
+            sigma = self.stdHeight
+            heights = np.random.normal(mu, sigma, self.numberOfEmitters)
         elif(heightDistribution == 'uniform'):
-            low = self.mu_height - np.sqrt(12) * self.sigma_height * 0.5
-            high = self.mu_height + np.sqrt(12) * self.sigma_height * 0.5
-            heights = np.random.uniform(low, high, self.N_emitters)
+            low = self.averageHeight - np.sqrt(12) * self.stdHeight * 0.5
+            high = self.averageHeight + np.sqrt(12) * self.stdHeight * 0.5
+            heights = np.random.uniform(low, high, self.numberOfEmitters)
         elif(heightDistribution == 'exponential'):
-            heights = np.random.exponential(self.mu_height, self.N_emitters)
+            heights = np.random.exponential(self.averageHeight, self.numberOfEmitters)
         else:
             print ("wrong distribution for heights")
         
         if (radiiDistribution == "lognormal"):
-            mu = np.log(self.mu_radii**2/np.sqrt(self.sigma_radii**2 + self.mu_radii**2))
-            sigma = np.sqrt(np.log(1+self.sigma_radii**2/self.mu_radii**2))
-            radii = np.random.lognormal(mu, sigma, self.N_emitters)
+            mu = np.log(self.averageRadius**2/np.sqrt(self.stdRadius**2 + self.averageRadius**2))
+            sigma = np.sqrt(np.log(1+self.stdRadius**2/self.averageRadius**2))
+            radii = np.random.lognormal(mu, sigma, self.numberOfEmitters)
         elif(radiiDistribution == 'normal'):
-            mu = self.mu_radii
-            sigma = self.sigma_radii
-            radii = np.random.normal(mu, sigma, self.N_emitters)
+            mu = self.averageRadius
+            sigma = self.stdRadius
+            radii = np.random.normal(mu, sigma, self.numberOfEmitters)
         elif(radiiDistribution == 'uniform'):
-            low = self.mu_radii - np.sqrt(12) * self.sigma_radii * 0.5
-            high = self.mu_radii + np.sqrt(12) * self.sigma_radii * 0.5
-            radii = np.random.uniform(low, high, self.N_emitters)
+            low = self.averageRadius - np.sqrt(12) * self.stdRadius * 0.5
+            high = self.averageRadius + np.sqrt(12) * self.stdRadius * 0.5
+            radii = np.random.uniform(low, high, self.numberOfEmitters)
         elif(radiiDistribution == 'exponential'):
-            curvatures = np.random.exponential(1./self.mu_radii, self.N_emitters)
+            curvatures = np.random.exponential(1./self.averageRadius, self.numberOfEmitters)
             radii = 1./curvatures
         else:
             print ("wrong distribution for radii"    )
@@ -79,13 +66,13 @@ class MultiEmitter():
         self.radii = radii[isgood]
         self.heights = heights[isgood]
         self.betas = betas[isgood]
-        self.N_emitters = len(self.betas)
+        self.numberOfEmitters = len(self.betas)
         
         if appendMaxValues:
             self.betas = np.append(self.betas, maxBetaValue)
             self.radii = np.append(self.radii, min(self.radii))
             self.heights = np.append(self.heights, self.betas[-1] * self.radii[-1])
-            self.N_emitters += 1
+            self.numberOfEmitters += 1
             
         
         self.currents = np.copy(self.betas) * 0.
@@ -97,26 +84,26 @@ class MultiEmitter():
         if(beta_dist == 'normal'):
             mu = 0.5 * (min_beta + max_beta)
             sigma = (max_beta - min_beta) * 0.25
-            self.betas = np.random.normal(mu, sigma, self.N_emitters)
+            self.betas = np.random.normal(mu, sigma, self.numberOfEmitters)
         elif(beta_dist == 'uniform'):
-            low = self.mu_height - np.sqrt(12) * self.sigma_height * 0.5
-            high = self.mu_height + np.sqrt(12) * self.sigma_height * 0.5
-            self.betas = np.random.uniform(min_beta, max_beta, self.N_emitters)
+            low = self.averageHeight - np.sqrt(12) * self.stdHeight * 0.5
+            high = self.averageHeight + np.sqrt(12) * self.stdHeight * 0.5
+            self.betas = np.random.uniform(min_beta, max_beta, self.numberOfEmitters)
         else:
             print ("wrong distribution for betas")
             
         if (r_dist == "lognormal"):
-            mu = np.log(self.mu_radii**2/np.sqrt(self.sigma_radii**2 + self.mu_radii**2))
-            sigma = np.sqrt(np.log(1+self.sigma_radii**2/self.mu_radii**2))
-            self.radii = np.random.lognormal(mu, sigma, self.N_emitters)
+            mu = np.log(self.averageRadius**2/np.sqrt(self.stdRadius**2 + self.averageRadius**2))
+            sigma = np.sqrt(np.log(1+self.stdRadius**2/self.averageRadius**2))
+            self.radii = np.random.lognormal(mu, sigma, self.numberOfEmitters)
         elif(r_dist == 'normal'):
-            mu = self.mu_radii
-            sigma = self.sigma_radii
-            self.radii = np.random.normal(mu, sigma, self.N_emitters)
+            mu = self.averageRadius
+            sigma = self.stdRadius
+            self.radii = np.random.normal(mu, sigma, self.numberOfEmitters)
         elif(r_dist == 'uniform'):
-            low = self.mu_radii - np.sqrt(12) * self.sigma_radii * 0.5
-            high = self.mu_radii + np.sqrt(12) * self.sigma_radii * 0.5
-            self.radii = np.random.uniform(low, high, self.N_emitters)
+            low = self.averageRadius - np.sqrt(12) * self.stdRadius * 0.5
+            high = self.averageRadius + np.sqrt(12) * self.stdRadius * 0.5
+            self.radii = np.random.uniform(low, high, self.numberOfEmitters)
         else:
             print ("wrong distribution for radii")
             
@@ -126,12 +113,10 @@ class MultiEmitter():
         
            
     def emit(self, Ffar):
-        for i in range(len(self.betas)):
-            self.emitter.F = self.betas[i] * Ffar
-            self.emitter.R = self.radii[i]
-            self.emitter.cur_dens()
-            self.currents[i] = self.emitter.Jem * self.areas[i]
-            
+        self.emitterModel.setParameters(field = Ffar * self.betas, radius = self.radii)
+        self.emitterModel.calculateCurrentDensity()
+
+        self.currents = np.array(self.emitterModel.getCurrentDensity()) * np.pi * self.radii**2            
         return sum(self.currents)   
         
         
@@ -169,7 +154,8 @@ class MultiEmitter():
 
         
 if (__name__ == "__main__"): #some testing operations
-    me = MultiEmitter(N=200)
+    getelecModel = gt.GETELECModel(emitterType="metal", workFunction=4.5, temperature=300, gamma=10., effectiveMassConduction=1.)
+    me = MultiEmitter(emitterModel= getelecModel)
     me.getEmittersFromBetaDistribution(min_beta=50, max_beta=110, beta_dist="normal", r_dist="lognormal")
     
     farFields = np.linspace(0.02, 0.08, 128)
