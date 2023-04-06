5193d7e3ba2a94a26ba0db4120ffb4cdc5ef3897
Andreas Kyritsakis <akyritsos1@gmail.com>
2023-04-06 15:59:43 +0300

    implemented and tested two emitter fitter

--- src/IVDataFitter.py
+++ src/IVDataFitter.py
@@ -2,10 +2,9 @@ import getelec as gt
 import numpy as np
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
-import copy
 
-class IVDataFitter:
-    
+
+class generalFitter:
     emitterModel:gt.GETELECModel
     fittingParameters: dict
     initialParameters: dict
@@ -13,6 +12,7 @@ class IVDataFitter:
     maxParameters: dict
     voltageData:np.ndarray
     currentData:np.ndarray
+    optimizationData:opt.OptimizeResult
 
     def __init__(self, emitterModel:gt.GETELECModel = gt.GETELECModel()) -> None:
         self.emitterModel = emitterModel
@@ -20,6 +20,19 @@ class IVDataFitter:
         self.minParameters = {"fieldConversionFactor": 1.}
         self.initialParameters = {"fieldConversionFactor": 1.}
         self.maxParameters = {"fieldConversionFactor": 1.}
+        self.preFactor = 1.
+     
+    def setIVdata(self, voltageData:np.ndarray, currentData:np.ndarray) -> None:
+        self.voltageData = voltageData
+        self.currentData = currentData
+
+    def getFittingError(self) -> None:
+        return self.optimizationData.cost
+
+class IVDataFitter(generalFitter):
+    
+    def __init__(self, emitterModel:gt.GETELECModel = gt.GETELECModel()) -> None:
+        super().__init__(emitterModel)
 
     def currentDensityforVoltages(self, voltageArray:np.ndarray) -> np.ndarray:
         """ Calculates and returns the current density for a given array of voltages, assuming a field conversion factor 
@@ -35,11 +48,12 @@ class IVDataFitter:
         self.emitterModel.setParameters(field=self.fittingParameters["fieldConversionFactor"] * voltageArray)
         
         #set all other parameters that need to be fitted
-        self.emitterModel.setParameters(**{key: self.fittingParameters[key] for key in self.fittingParameters if key !="fieldConversionFactor"})
+        self.emitterModel.setParameters(**{key: self.fittingParameters[key] for key in self.fittingParameters if key in ["radius", "gamma", "temperature", "workFunction"]})
     
         self.emitterModel.calculateCurrentDensity()
-        
+                
         return np.array(self.emitterModel.getCurrentDensity())
+
     
     def logCurrentDensityError(self, parameterList) -> float:
         """ Calculates the error between a given I-V curve and the one calculated by GETELEC for a given set of parameters.
@@ -69,9 +83,19 @@ class IVDataFitter:
         error = np.log(self.calculatedCurrentDensities / self.currentData) + logOfPrefactor
         return error
 
-    def setIVdata(self, voltageData:np.ndarray, currentData:np.ndarray) -> None:
-        self.voltageData = voltageData
-        self.currentData = currentData
+    
+    def fitIVCurve(self) -> None:
+        """Performs the fitting"""
+        self.optimizationData = opt.least_squares(fun = self.logCurrentDensityError, x0 = list(self.initialParameters.values()), \
+            bounds =(list(self.minParameters.values()), list(self.maxParameters.values())), method = 'trf', jac = '3-point')
+        
+        i = 0
+        for parameterName in self.fittingParameters:
+            self.fittingParameters[parameterName] = self.optimizationData.x[i]
+            i += 1
+        
+        self.fittedCurrent = self.preFactor * self.currentDensityforVoltages(self.voltageData)
+
 
     def setParameterRange(self, field:list = [1., 6., 15.], radius = 2000., gamma = 10., workFunction = 4.5, temperature = 300.) ->None:
 
@@ -118,18 +142,6 @@ class IVDataFitter:
         if "gamma" in self.fittingParameters:
             assert self.emitterModel.emitter.barrier.getDimension() == 3, "You cannot fit for Gamma if the tabulation dimension is < 3"
 
-    def fitIVCurve(self) -> None:
-        """Performs the fitting"""
-        self.optimizationData = opt.least_squares(fun = self.logCurrentDensityError, x0 = list(self.initialParameters.values()), \
-            bounds =(list(self.minParameters.values()), list(self.maxParameters.values())), method = 'trf', jac = '3-point')
-        
-        i = 0
-        for parameterName in self.fittingParameters:
-            self.fittingParameters[parameterName] = self.optimizationData.x[i]
-            i += 1
-        
-        self.fittedCurrent = self.preFactor * self.currentDensityforVoltages(self.voltageData)
-
     def getOptCurrentCurve(self, voltageData:np.ndarray = None) -> np.ndarray:
         """Calculates and returns the current curve for a given input voltage data array
         Args:
@@ -141,9 +153,6 @@ class IVDataFitter:
             return self.fittedCurrent
         else:
             return self.preFactor * self.currentDensityforVoltages(voltageData)
-        
-    def getFittingError(self) -> None:
-        return self.optimizationData.cost
     
     def printFittingData(self) -> None:
         print("Optimum parameters: ", self.fittingParameters)
@@ -171,22 +180,136 @@ class IVDataFitter:
         self.orthodoxyMessage = "The data are within field emission orthodoxy limits"
         return "orthodox"
 
-class twoEmitterFitter:
+class twoEmitterFitter(generalFitter):
     def __init__(self, emitterModel: gt.GETELECModel = gt.GETELECModel()) -> None:
+        super().__init__(emitterModel)
 
         self.fittingParameters = {"fieldConversionFactor": 1., "secondFieldConversionFactor": 2., "prefactor": 1., "secondPrefactor": 1.}
         self.minParameters ={"fieldConversionFactor": 1., "secondFieldConversionFactor": 2., "prefactor": 1., "secondPrefactor": 1.}
         self.initialParameters = {"fieldConversionFactor": 1., "secondFieldConversionFactor": 2., "prefactor": 1., "secondPrefactor": 1.}
         self.maxParameters = {"fieldConversionFactor": 1., "secondFieldConversionFactor": 2., "prefactor": 1., "secondPrefactor": 1.}
 
-        self.firstEmitter = emitterModel
+    def setParameterRange(self, field:list = [1.e-5, 5., 15.], radius = 2000., gamma = 10., workFunction = 4.5, temperature = 300., prefactor = [0.01, 100., 1.e10], secondPrefactor = [0.01, 100., 1.e10]) ->None:
 
-        self.secondEmitter = copy.deepcopy(self.firstEmitter)
+        """Sets the range of the parameters that will be fitted (variables) and the value of those that will be fixed
+        
+        Args: 
+            field: list of 3 floats. Minimum, initial, and maximum value of the field that is allowed for fitting
+            radius, gamma, workFunction, temperature: float or list of 3 floats. If single float, it means that the parameter is considered fixed. If list, it is variable and it works as for field. Default values are 2000. [nm], 10., 4.5 [eV], 300 [K] correspondingly
+        """
+        self.minParameters ["fieldConversionFactor"] = field[0] / np.min(self.voltageData)
+        self.initialParameters["fieldConversionFactor"] = field[1] / np.mean(self.voltageData)
+        self.maxParameters["fieldConversionFactor"] = field[2] / np.max(self.voltageData)
+
+        self.minParameters ["secondFieldConversionFactor"] = field[0] / np.min(self.voltageData)
+        self.initialParameters["secondFieldConversionFactor"] = field[1] / np.mean(self.voltageData)
+        self.maxParameters["secondFieldConversionFactor"] = field[2] / np.max(self.voltageData)
+
+        #parameter arguments that are to be fixed
+        fixedParameterArgs = {key:value for key,value in locals().items() if type(value) in [float, int]}
+        
+        #parameter arguments that are to be 
+        fittingParameterArgs = {key:value for key,value in locals().items() if type(value) == list and key != "field"}
+
+        # set range for all parameters to be fitted
+        for key,value in fittingParameterArgs.items(): 
+            self.fittingParameters[key] = value[1]
+            self.minParameters[key] = value[0]
+            self.initialParameters[key] = value[1]
+            self.maxParameters[key] = value[2]
+        
+        # set the value of the parameters that are fixed
+        self.emitterModel.setParameters(**fixedParameterArgs)
+
+        #remove all fixed parameters from the fittingParameter dictionary
+        for key in fixedParameterArgs:
+            try:
+                self.fittingParameters.pop(key)
+                self.initialParameters.pop(key)
+                self.minParameters.pop(key)
+                self.maxParameters.pop(key)
+            except KeyError:
+                pass
+
+    def currentDensityforVoltages(self, voltageArray:np.ndarray) -> np.ndarray:
+        """ Calculates and returns the current density for a given array of voltages, assuming a field conversion factor 
+        (F=fieldConversionfactor * Voltage). 
+        
+        Parameters:
+            voltageArray: array of Voltages (Volts)
+        Returns:
+            currentDensity: Array of the resulting current densities for each voltage element
+        """
+
+        #set the fields
+        self.emitterModel.setParameters(field=self.fittingParameters["fieldConversionFactor"] * voltageArray)
+        self.emitterModel.calculateCurrentDensity()
+        self.currentDensityFromFristEmitter = np.array(self.emitterModel.getCurrentDensity())
+
+        self.emitterModel.setParameters(field=self.fittingParameters["secondFieldConversionFactor"] * voltageArray)
+        self.emitterModel.calculateCurrentDensity()
+        self.currentDensityFromSecondEmitter = np.array(self.emitterModel.getCurrentDensity())
+
+        
+        return self.fittingParameters["prefactor"] * self.currentDensityFromFristEmitter + self.fittingParameters["secondPrefactor"] * self.currentDensityFromSecondEmitter
+    
+    def logCurrentDensityError(self, parameterList) -> float:
+        """ Calculates the error between a given I-V curve and the one calculated by GETELEC for a given set of parameters.
+        The main usage of this function is to be called by external minimization function in order to find the parameterList that
+        gives the best fit to the given I-V
+
+        Parameters:
+            parameterList: list of emitter parameters (fieldConversionFactor, radius, gamma, workFunction, temperature)
+        Returns:
+            array of relative errors (in logarithmic scale) between the calculated and the given current densities, after normalizing with a multiplication
+            factor that forces them to match at their maximum
+        """
+
+        assert len(parameterList) == len(self.fittingParameters), "parameterList has a length of %d while it should have %d"%(len(parameterList), len(self.fittingParameters))
+        
+        #copy parameter list into the parameter dictionary
+        i = 0
+        for paramName in self.fittingParameters:
+            self.fittingParameters[paramName] = parameterList[i]
+            i += 1
+
+        self.calculatedCurrentDensities = self.currentDensityforVoltages(self.voltageData)
+        return np.log(self.calculatedCurrentDensities / self.currentData)
+
+
+    def fitIVCurve(self) -> None:
+        """Performs the fitting"""
+        self.optimizationData = opt.least_squares(fun = self.logCurrentDensityError, x0 = list(self.initialParameters.values()), \
+            bounds =(list(self.minParameters.values()), list(self.maxParameters.values())), method = 'trf', jac = '3-point')
+        
+        i = 0
+        for parameterName in self.fittingParameters:
+            self.fittingParameters[parameterName] = self.optimizationData.x[i]
+            i += 1
+        
+        self.fittedCurrent = self.currentDensityforVoltages(self.voltageData)
+
+    def printFittingData(self) -> None:
+        print("Optimum parameters: ", self.fittingParameters)
+        print("Fitting Error (relative): ", self.getFittingError())
+
+
+    def getOptCurrentCurve(self, voltageData:np.ndarray = None) -> np.ndarray:
+        """Calculates and returns the current curve for a given input voltage data array
+        Args:
+            voltageData[optional]. array of input voltage values [V]. If not given, the internal voltageData array is used
+        Returns:
+            Array of currents calculated by the optimized system 
+        """
+        if (voltageData is None):
+            return self.fittedCurrent
+        else:
+            return self.preFactor * self.currentDensityforVoltages(voltageData)
 
 if (__name__ == "__main__"): #some testing operations
     
 
-    fitter = IVDataFitter()
+    ivFitter = IVDataFitter()
 
 
     voltageData = np.array([6666.66666667, 6528.49740933, 6395.93908629, 6268.65671642, \
@@ -223,29 +346,63 @@ if (__name__ == "__main__"): #some testing operations
        7.33204336e-13, 5.23857148e-13, 3.73798141e-13, 2.66375053e-13])
 
 
-    fitter.setIVdata(voltageData=voltageData, currentData=currentData)
-    fitter.setParameterRange()
-    fitter.fitIVCurve()
-    fittedCurrent = fitter.getOptCurrentCurve(voltageData)
+    ivFitter.setIVdata(voltageData=voltageData, currentData=currentData)
+    ivFitter.setParameterRange()
+    ivFitter.fitIVCurve()
+    fittedCurrent = ivFitter.getOptCurrentCurve(voltageData)
     plt.semilogy(1./voltageData, currentData, '.', label="data" )
-    plt.semilogy(1./voltageData, fittedCurrent, label = "beta=%.2g, err=%.2g"%(fitter.fittingParameters["fieldConversionFactor"], fitter.getFittingError()))
+    plt.semilogy(1./voltageData, fittedCurrent, label = "beta=%.2g, err=%.2g"%(ivFitter.fittingParameters["fieldConversionFactor"], ivFitter.getFittingError()))
 
-    fitter.setParameterRange(radius=[1., 10., 2000.])
+    ivFitter.setParameterRange(radius=[1., 10., 2000.])
 
-    fitter.fitIVCurve()
-    fittedCurrent = fitter.getOptCurrentCurve()
-    plt.semilogy(1./voltageData, fittedCurrent, label = "beta = %.2g, R = %.2g, err=%.2g"%(fitter.fittingParameters["fieldConversionFactor"], fitter.fittingParameters["radius"], fitter.getFittingError()))
+    ivFitter.fitIVCurve()
+    fittedCurrent = ivFitter.getOptCurrentCurve()
+    plt.semilogy(1./voltageData, fittedCurrent, label = "beta = %.2g, R = %.2g, err=%.2g"%(ivFitter.fittingParameters["fieldConversionFactor"], ivFitter.fittingParameters["radius"], ivFitter.getFittingError()))
 
 
 
-    fitter.setParameterRange(workFunction=[2.5, 4.5, 7.], radius=[1., 10., 200.])
+    ivFitter.setParameterRange(workFunction=[2.5, 4.5, 7.], radius=[1., 10., 200.])
 
-    fitter.fitIVCurve()
-    fittedCurrent = fitter.getOptCurrentCurve()
-    plt.semilogy(1./voltageData, fittedCurrent, label = "beta = %.2g, R = %.2g, W = %.2g, err = %.2g"%(fitter.fittingParameters["fieldConversionFactor"], fitter.fittingParameters["radius"], fitter.fittingParameters["workFunction"], fitter.getFittingError()))
+    ivFitter.fitIVCurve()
+    fittedCurrent = ivFitter.getOptCurrentCurve()
+    plt.semilogy(1./voltageData, fittedCurrent, label = "beta = %.2g, R = %.2g, W = %.2g, err = %.2g"%(ivFitter.fittingParameters["fieldConversionFactor"], ivFitter.fittingParameters["radius"], ivFitter.fittingParameters["workFunction"], ivFitter.getFittingError()))
 
     plt.legend()
 
     plt.savefig("fittedcurve.png")
+    plt.close()
 
-    fitter.printFittingData()
+    ivFitter.printFittingData()
+
+
+    voltageData = 1./np.array([0.05401709, 0.05757835, 0.05907407, 0.0605698 , 0.06185185, \
+       0.06349003, 0.06519943, 0.06655271, 0.06733618, 0.06840456, \
+       0.06990028, 0.07153846, 0.07353276, 0.07566952, 0.07766382,\
+       0.07958689, 0.0810114 , 0.0824359 , 0.08407407, 0.0855698 ,\
+       0.08777778, 0.08934473, 0.09112536, 0.09247863, 0.09447293,\
+       0.09603989, 0.09817664, 0.09988604, 0.10173789, 0.10330484,\
+       0.10529915, 0.10665242, 0.10814815, 0.10935897, 0.11064103,\
+       0.1122792 , 0.1139886 ])
+    
+    currentData = 1.e-9 * np.array([1.32987103e+06, 6.30957344e+05, 4.64158883e+05, 3.34048498e+05,\
+       2.68269580e+05, 1.93069773e+05, 1.35935639e+05, 1.04483478e+05,\
+       8.76712387e+04, 7.04075201e+04, 5.17947468e+04, 3.64673967e+04,\
+       2.40409918e+04, 1.55051578e+04, 9.78309319e+03, 6.59246176e+03,\
+       4.84969343e+03, 3.56763941e+03, 2.74217545e+03, 1.93069773e+03,\
+       1.30102522e+03, 9.16019591e+02, 6.73862717e+02, 5.06712835e+02,\
+       3.56763941e+02, 2.56757897e+02, 1.65595152e+02, 1.19176459e+02,\
+       8.76712387e+01, 6.59246176e+01, 4.64158883e+01, 3.56763941e+01,\
+       2.92864456e+01, 2.35195264e+01, 1.88881958e+01, 1.55051578e+01,\
+       1.14062492e+01])
+    
+    ivFitter = twoEmitterFitter()
+
+    ivFitter.setIVdata(voltageData=voltageData, currentData=currentData)
+    ivFitter.setParameterRange()
+    ivFitter.fitIVCurve()
+    fittedCurrent = ivFitter.getOptCurrentCurve(voltageData)
+    plt.semilogy(1./voltageData, currentData, '.', label="data" )
+    plt.semilogy(1./voltageData, fittedCurrent, label = "beta1=%.2g, beta2:%.2g, pref1:%.2g, pref2:%.2g, err=%.2g"%(ivFitter.fittingParameters["fieldConversionFactor"], ivFitter.fittingParameters["secondFieldConversionFactor"], ivFitter.fittingParameters["prefactor"], ivFitter.fittingParameters["secondPrefactor"] , ivFitter.getFittingError()))
+
+    ivFitter.printFittingData()
+    plt.savefig("twoEmitterFitting.png")
