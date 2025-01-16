101becaef0a0306264ef919f0caac60c0effa6f5
Andreas <akyritsos1@gmail.com>
2025-01-16 05:59:53 +0200

    adding configuration file reader. Not connected to the code yet

---
+++ include/Config.h
@@ -0,0 +1,124 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+/** @brief Class to initialize and read configuration parameters from configuration file */
+class Config {
+public:
+
+    /** @brief initializes configuration parameters */
+    Config(string fileName = "GetelecConfig.txt"){
+        transmissionSolverParams = TransmissionSolverParams();
+        bandEmitterParams = BandEmitterParams();
+        read_all(fileName);
+    }
+
+    /**
+     * @brief Read the configuration parameters from input script 
+     * @param file_name path to the configuration file
+    */
+    void read_all(const string& file_name);
+
+    /** 
+     * @brief Read configuration parameter of type string
+     * @param paramName the name of the parameter 
+     * @param arg reference to the parameter to be read
+    */
+    int read_command(string paramName, string& arg);
+
+    /** 
+     * @brief Read configuration parameter of type boolean
+     * @param paramName the name of the parameter 
+     * @param arg reference to the parameter to be read  
+    */
+    int read_command(string paramName, bool& arg);
+
+    /** 
+     * @brief Read configuration parameter of type unsigned int
+     * @param paramName the name of the parameter 
+     * @param arg reference to the parameter to be read  
+    */
+    int read_command(string paramName, unsigned int& arg);
+
+    /** 
+     * @brief Read configuration parameter of type int
+     * @param paramName the name of the parameter 
+     * @param arg reference to the parameter to be read  
+    */
+    int read_command(string paramName, int& arg);
+
+    /** 
+     * @brief Read configuration parameter of type double
+     * @param paramName the name of the parameter 
+     * @param arg reference to the parameter to be read  
+    */
+    int read_command(string paramName, double& arg);
+
+
+    /** 
+     * @brief Read two configuration parameter of type double
+     * @param paramName the name of the parameter 
+     * @param arg reference to the first parameter to be read 
+     * @param arg2 reference to the second parameter to be read 
+    */
+    int read_command(string paramName, double& arg, double& arg2);
+
+
+    /** 
+     * @brief Read a configuration parameter that is a series of strings
+     * @param paramName the name of the parameter 
+     * @param args reference to the vector of parameters to be read  
+    */
+    int read_command(string paramName, vector<string>& args);
+
+    /** @brief Look up the configuration parameter with several double arguments */
+    int read_command(string paramName, vector<double>& args);
+
+    /** @brief Print the stored commands and parameters */
+    void print_data();
+
+    /** @brief Struct that contains all the configuration parameters related to the TransmissionSolver class */
+    struct TransmissionSolverParams {
+        double relativeTolerance = 1.e-5;
+        double absoluteTolerance = 1.e-5;
+        string stepType = "rk8pd";
+        int maxSteps = 4096;
+        int minSteps = 64;
+        int stepExpectedForInitialStep = 64;
+    } transmissionSolverParams;
+
+    /** @brief Parameters releated to the BandEmitter Class */
+    struct BandEmitterParams {
+        double relativeTolerance = 1.e-4;
+        double absoluteTolerance = 1.e-12;
+        int maxSteps = 4096;
+        int minSteps = 16;
+        int stepExpectedForInitialStep = 256;
+    } bandEmitterParams;
+
+private:
+    vector<vector<string>> data; ///< commands and their arguments found from the input script
+    string file_name;            ///< path to configuration file
+
+    const string comment_symbols = "!#%";
+    const string data_symbols = "+-/*_.0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ()";
+
+    /** Check for the obsolete commands from the buffered commands */
+    void check_obsolete(const string& file_name);
+
+    /** Check for the obsolete commands that are similar to valid ones */
+    void check_changed(const string& command, const string& substitute);
+
+    /** Read the commands and their arguments from the file and store them into the buffer */
+    void parse_file(const string& file_name);
+
+    /** Remove the noise from the beginning of the string */
+    void trim(string& str);
+};
+
+#endif /* CONFIG_H_ */
---
+++ src/Config.cpp
@@ -0,0 +1,224 @@
+/*
+ * Config.cpp
+ *
+ *  Created on: 11.11.2016
+ *      Author: veske
+ */
+
+#include <fstream>
+#include <algorithm>
+
+#include "Config.h"
+#include <cassert>
+#include <iostream>
+#include <sstream>
+
+
+using namespace std;
+
+// Remove the noise from the beginning of the string
+void Config::trim(string& str) {
+    str.erase(0, str.find_first_of(comment_symbols + data_symbols));
+}
+
+void Config::read_all(const string& fname) {
+    if (fname == "") return;
+    file_name = fname;
+
+    // Store the commands and their arguments
+    parse_file(fname);
+
+    // Modify the parameters that are specified in input script
+    read_command("transmissionSolverRelativeTolerance", transmissionSolverParams.relativeTolerance);
+    read_command("transmissionSolverAbsoluteTolerance", transmissionSolverParams.absoluteTolerance);
+    read_command("transmissionSolverStepType", transmissionSolverParams.stepType);
+    read_command("transmissionSolverMaxSteps", transmissionSolverParams.maxSteps);
+    read_command("transmissionSolverMinSteps", transmissionSolverParams.minSteps);
+    read_command("transmissionSolverStepExpectedForInitialStep", transmissionSolverParams.stepExpectedForInitialStep);
+
+    read_command("bandEmitterRelativeTolerance", bandEmitterParams.relativeTolerance);
+    read_command("bandEmitterAbsoluteTolerance", bandEmitterParams.absoluteTolerance);
+    read_command("bandEmitterMaxSteps", bandEmitterParams.maxSteps);
+    read_command("bandEmitterMinSteps", bandEmitterParams.minSteps);
+    read_command("bandEmitterStepExpectedForInitialStep", bandEmitterParams.stepExpectedForInitialStep);
+
+}
+
+void Config::parse_file(const string& file_name) {
+    ifstream file(file_name);
+
+    if(!file.is_open()){
+        std::cerr << "Config file " << file_name << " not found." << endl;
+        return;
+    }    
+    string line;
+    data.clear();
+
+    // loop through the lines in a file
+    while (getline(file, line)) {
+        line += " "; // needed to find the end of line
+
+        bool line_started = true;
+        // store the command and its parameters from non-empty and non-pure-comment lines
+        while(line.size() > 0) {
+            trim(line);
+            int i = line.find_first_not_of(data_symbols);
+            if (i <= 0) break;
+
+            if (line_started && line.substr(0, i) == "femocs_end") return;
+            if (line_started) data.push_back({});
+            if (line_started) {
+                // force all the characters in a command to lower case
+                string command = line.substr(0, i);
+                std::transform(command.begin(), command.end(), command.begin(), ::tolower);
+                data.back().push_back(command);
+            } else {
+                data.back().push_back( line.substr(0, i) );
+            }
+
+            line = line.substr(i);
+            line_started = false;
+        }
+    }
+}
+
+void Config::check_obsolete(const string& command) {
+    for (const vector<string>& cmd : data)
+        if (cmd[0] == command) {
+            cout << "Command '"  << command  << "' is obsolete! You can safely remove it!" << endl;
+            return;
+        }
+}
+
+void Config::check_changed(const string& command, const string& substitute) {
+    for (const vector<string>& cmd : data)
+        if (cmd[0] == command) {
+            cout << "Command '" << command << "' has changed!"
+                    " It is similar yet different to the command '" << substitute << "'!" << endl;
+            return;
+        }
+}
+
+int Config::read_command(string param, string& arg) {
+    // force the parameter to lower case
+    std::transform(param.begin(), param.end(), param.begin(), ::tolower);
+    // loop through all the commands that were found from input script
+    for (const vector<string>& str : data)
+        if (str.size() >= 2 && str[0] == param) {
+            arg = str[1]; return 0;
+        }
+    return 1;
+}
+
+int Config::read_command(string param, bool& arg) {
+    // force the parameter to lower case
+    std::transform(param.begin(), param.end(), param.begin(), ::tolower);
+    // loop through all the commands that were found from input script
+    for (const vector<string>& str : data)
+        if (str.size() >= 2 && str[0] == param) {
+            istringstream is1(str[1]);
+            istringstream is2(str[1]);
+            bool result;
+            // try to parse the bool argument in text format
+            if (is1 >> std::boolalpha >> result) { arg = result; return 0; }
+            // try to parse the bool argument in numeric format
+            else if (is2 >> result) { arg = result; return 0; }
+            return 1;
+        }
+    return 1;
+}
+
+int Config::read_command(string param, unsigned int& arg) {
+    // force the parameter to lower case
+    std::transform(param.begin(), param.end(), param.begin(), ::tolower);
+    // loop through all the commands that were found from input script
+    for (const vector<string>& str : data)
+        if (str.size() >= 2 && str[0] == param) {
+            istringstream is(str[1]); int result;
+            if (is >> result) { arg = result; return 0; }
+            return 1;
+        }
+    return 1;
+}
+
+int Config::read_command(string param, int& arg) {
+    // force the parameter to lower case
+    std::transform(param.begin(), param.end(), param.begin(), ::tolower);
+    // loop through all the commands that were found from input script
+    for (const vector<string>& str : data)
+        if (str.size() >= 2 && str[0] == param) {
+            istringstream is(str[1]); int result;
+            if (is >> result) { arg = result; return 0; }
+            return 1;
+        }
+    return 1;
+}
+
+int Config::read_command(string param, double& arg) {
+    // force the parameter to lower case
+    std::transform(param.begin(), param.end(), param.begin(), ::tolower);
+    // loop through all the commands that were found from input script
+    for (const vector<string>& str : data)
+        if (str.size() >= 2 && str[0] == param) {
+            istringstream is(str[1]); double result;
+            if (is >> result) { arg = result; return 0; }
+            return 1;
+        }
+    return 1;
+}
+
+int Config::read_command(string param, double& arg, double& arg2) {
+    // force the parameter to lower case
+    std::transform(param.begin(), param.end(), param.begin(), ::tolower);
+    // loop through all the commands that were found from input script
+    for (const vector<string>& str : data)
+        if (str.size() >= 2 && str[0] == param) {
+            istringstream is(str[1]); double result;
+            if (is >> result) { arg = result; arg2 = result; return 0; }
+            return 1;
+        }
+    return 1;
+}
+
+int Config::read_command(string param, vector<string>& args) {
+    // force the parameter to lower case
+    std::transform(param.begin(), param.end(), param.begin(), ::tolower);
+    // loop through all the commands that were found from input script
+    int n_read_args = 0;
+    for (const vector<string>& str : data)
+        if (str.size() >= 2 && str[0] == param)
+            for (unsigned i = 0; i < args.size() && i < (str.size()-1); ++i) {
+                args[i] = str[i+1];
+		    n_read_args++;
+            }
+    return n_read_args;
+}
+
+int Config::read_command(string param, vector<double>& args) {
+    // force the parameter to lower case
+    std::transform(param.begin(), param.end(), param.begin(), ::tolower);
+    // loop through all the commands that were found from input script
+    int n_read_args = 0;
+    for (const vector<string>& str : data)
+        if (str.size() >= 2 && str[0] == param)
+            for (unsigned i = 0; i < args.size() && i < (str.size()-1); ++i) {
+                istringstream is(str[i+1]);
+                double result;
+                if (is >> result) { args[i] = result; n_read_args++; }
+            }
+    return n_read_args;
+}
+
+void Config::print_data() {
+    const int cmd_len = 20;
+
+    for (const vector<string>& line : data) {
+        for (const string& ln : line) {
+            int str_len = ln.length();
+            int whitespace_len = max(1, cmd_len - str_len);
+            cout << ln << string(whitespace_len, ' ');
+        }
+
+        cout << endl;
+    }
+}
