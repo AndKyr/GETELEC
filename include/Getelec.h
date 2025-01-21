4b44d9eec0d85be598e92b6974558e287d574f57
Andreas <akyritsos1@gmail.com>
2025-01-21 10:02:20 +0200

    fixing getelec workflow. tested and works

--- include/Getelec.h
+++ include/Getelec.h
@@ -18,57 +18,59 @@ public:
     ~Getelec(){}
 
     void setField(double field_ = 5.) { field = field_; }
-    void setField(std::vector<double>& fieldsVector_) { fieldsVector.swap(fieldsVector_); }
+    void setField(std::vector<double>* fieldsVector_) { fieldsVector = fieldsVector_; }
 
     void setRadius(double radius_ = 1.e4) { radius = radius_; }
-    void setRadius(std::vector<double>& radiiVector_) { radiiVector.swap(radiiVector_); }
+    void setRadius(std::vector<double>* radiiVector_) { radiiVector = radiiVector_; }
 
     void setGamma(double gamma_ = 10.) { gamma = gamma_; }
-    void setGamma(std::vector<double>& gammasVector_) { gammasVector.swap(gammasVector_); }
+    void setGamma(std::vector<double>* gammasVector_) { gammasVector = gammasVector_; }
 
     void setkT(double kT_ = .025) { kT = kT_; }
-    void setkT(std::vector<double>& kTVector_) { kTVector.swap(kTVector_); }
+    void setkT(std::vector<double>* kTVector_) { kTVector = kTVector_; }
 
     void setWorkFunction(double workFunction_ = 4.5) { workFunction = workFunction_; }
-    void setWorkFunction(std::vector<double>& workFunctionVector_) { workFunctionVector.swap(workFunctionVector_); }
+    void setWorkFunction(std::vector<double>* workFunctionVector_) { workFunctionVector = workFunctionVector_; }
 
     void setBandDepth(double bandDepth_ = 10.) { bandDepth = bandDepth_; }
-    void setBandDepth(std::vector<double>& bandDepthVector_) { bandDepthVector.swap(bandDepthVector_); } 
+    void setBandDepth(std::vector<double>* bandDepthVector_) { bandDepthVector = bandDepthVector_; } 
 
     void setEffectiveMass(double effectiveMass_ = 1.) { effectiveMass = effectiveMass_; }
-    void setEffectiveMass(vector<double>& effectiveMassVector_) { effectiveMassVector.swap(effectiveMassVector_); }   
+    void setEffectiveMass(vector<double>* effectiveMassVector_) { effectiveMassVector = effectiveMassVector_; }   
 
     /**
-     * @brief Calculate for a single case
+     * @brief Calculate for the i-th element of the array of inputs
+     * @param i The index of the element to calculate
+     * @param calculateSpectra If true, calculate the spectra
      */
     void runIteration(unsigned i = 0, bool calculateSpectra = false) {
         double workFunctionTmp = workFunction;
-        if (i < workFunctionVector.size())
-            workFunctionTmp = workFunctionVector[i];
+        if (workFunctionVector && i < workFunctionVector->size())
+            workFunctionTmp = (*workFunctionVector)[i];
 
         double kTTmp = kT;
-        if (i < kTVector.size())
-            kTTmp = kTVector[i];
+        if (kTVector && i < kTVector->size())
+            kTTmp = (*kTVector)[i];
 
         double effectiveMassTmp = effectiveMass;
-        if (i < effectiveMassVector.size())
-            effectiveMassTmp = effectiveMassVector[i];
+        if (effectiveMassVector && i < effectiveMassVector->size())
+            effectiveMassTmp = (*effectiveMassVector)[i];
         
         double bandDepthTmp = bandDepth;
-        if (i < bandDepthVector.size())
-            bandDepthTmp = bandDepthVector[i];
+        if (bandDepthVector && i < bandDepthVector->size())
+            bandDepthTmp = (*bandDepthVector)[i];
 
         double fieldTmp = field;
-        if (i < fieldsVector.size())
-            fieldTmp = fieldsVector[i];
+        if (fieldsVector && i < fieldsVector->size())
+            fieldTmp = (*fieldsVector)[i];
         
         double radiusTmp = radius;
-        if (i < radiiVector.size())
-            radiusTmp = radiiVector[i];
+        if (radiiVector && i < radiiVector->size())
+            radiusTmp = (*radiiVector)[i];
         
         double gammaTmp = gamma;
-        if (i < gammasVector.size())
-            gammaTmp = gammasVector[i];
+        if (gammasVector && i < gammasVector->size())
+            gammaTmp = (*gammasVector)[i];
         
 
         threadLocalBarrier.local().setBarrierParameters(fieldTmp, radiusTmp, gammaTmp);
@@ -86,6 +88,10 @@ public:
 
     }
 
+    /**
+     * @brief Run the calculation
+     * @param calculateSpectra If true, calculate the spectra
+     */
     void run(bool calculateSpectra = false){
         currentDensityVector.resize(getMaxIterations());
         nottinghamHeatVector.resize(getMaxIterations());
@@ -96,27 +102,31 @@ public:
     double getCurrentDensity(unsigned i = 0) const { return currentDensityVector[i]; }
     double getNottinghamHeat(unsigned i = 0) const { return nottinghamHeatVector[i]; }
     pair<const vector<double>&, const vector<double>&> getSpectra(unsigned i = 0) const { return spectra[i]; }
+    vector<pair<const vector<double>&, const vector<double>&>> getAllSpectra(unsigned i) const { return vector<pair<const vector<double>&, const vector<double>&>>(spectra.begin(), spectra.end()); }
+    const vector<double> getCurrentDensities() const { return vector<double>(currentDensityVector.begin(), currentDensityVector.end()); }
+    const vector<double> getNottinghamHeats() const { return vector<double>(nottinghamHeatVector.begin(), nottinghamHeatVector.end()); }
+
 private:
     double field = 5.; //< The electric field in V/nm.
-    vector<double> fieldsVector; //< The electric field in V/nm, multiple values to iterate over.
+    const vector<double>* fieldsVector = NULL; //< The electric field in V/nm, multiple values to iterate over.
 
     double radius = 1.e5; //< The radius of the emitter in nm.
-    vector<double> radiiVector; //< The radius of the emitter in nm, multiple values to iterate over.
+    const vector<double>* radiiVector = NULL; //< The radius of the emitter in nm, multiple values to iterate over.
 
     double gamma = 10.; //< Gamma parameter of the general barrier model.
-    vector<double> gammasVector; //< Gamma parameter of the general barrier model, multiple values to iterate over.
+    const vector<double>* gammasVector = NULL; //< Gamma parameter of the general barrier model, multiple values to iterate over.
 
     double kT = .025; //< Temperature in eV. 
-    vector<double> kTVector; //< Temperature in eV, multiple values to iterate over.
+    const vector<double>* kTVector = NULL; //< Temperature in eV, multiple values to iterate over.
 
     double workFunction = 4.5; //< Work function in eV.
-    vector<double> workFunctionVector; //< Work function in eV, multiple values to iterate over.
+    const vector<double>* workFunctionVector = NULL; //< Work function in eV, multiple values to iterate over.
 
     double bandDepth = 10.; //< Depth of the electronic band in eV.
-    vector<double> bandDepthVector; //< Depth of the electronic band in eV, multiple values to iterate over.
+    const vector<double>* bandDepthVector = NULL; //< Depth of the electronic band in eV, multiple values to iterate over.
 
     double effectiveMass = 1.; //< Effective mass of the electron.
-    vector<double> effectiveMassVector; //< Effective mass of the electron, multiple values to iterate over.
+    const vector<double>* effectiveMassVector = NULL; //< Effective mass of the electron, multiple values to iterate over.
 
     double currentDensity = 0.; //< The current density (output) in A/nm^2.
     tbb::concurrent_vector<double> currentDensityVector; //< The current density (output) in A/nm^2, multiple values to iterate over.
@@ -131,7 +141,12 @@ private:
     tbb::enumerable_thread_specific<BandEmitter> threadLocalEmitter; //< Thread-local instances of BandEmitter
 
     int getMaxIterations(){
-        return max(fieldsVector.size(), max(radiiVector.size(), max(gammasVector.size(), max(kTVector.size(), max(workFunctionVector.size(), max(bandDepthVector.size(), effectiveMassVector.size()))))));
+        const vector<const vector<double>*> allInputVectors = {fieldsVector, radiiVector, gammasVector, kTVector, workFunctionVector, bandDepthVector, effectiveMassVector};
+        int maxSize = 0;
+        for (auto inputVector : allInputVectors)
+            if (inputVector && inputVector->size() > maxSize)
+                maxSize = inputVector->size();
+        return maxSize;
     }
 
 };
